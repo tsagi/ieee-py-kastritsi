b5098b9552ea67a8fb54abc67aecf42680dfb468
Tasos Sangiotis <tsagiotis@gmail.com>
2015-03-30 21:01:00 +0300

    Imdb search by title

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import wolframalpha
-
+from imdbpie import Imdb
 
 def listify(texts):
     '''A method parsing the text from the wolfram alpha pod for the specific question.
@@ -20,6 +20,23 @@ def listify(texts):
     #Cut every row on the '|' and keep only the second item of the row
     items = [item.split('|')[1].strip() for item in items]
     return items    #Return the list
+
+def imdbit(title):
+    '''A method using imdb API to search by movie titles.
+
+    Args:
+      title:
+        The title of the movie
+
+    Returns:
+      The imdb id of the movie (www.imdb.com/title/theiddddd/)
+    '''
+
+    imdb = Imdb()   #Start imdb
+
+    movie = imdb.search_for_title(title) #do the search
+    m = dict(movie[0])  #save the most relevant movie in a dictionary
+    return m.get('imdb_id') #return the id
 
 
 def wolframquery(year):
@@ -46,7 +63,9 @@ def wolframquery(year):
         pod = res.pods[1]   #get the most relevant
         if pod.text:        #Do we have text?
             movies = listify(pod.text) #if yes create a list with the results
-            return movies  #return it
+            for movie in movies:        #for every movie
+                im_res[movie] = imdbit(movie)     #get the id and save it
+            return im_res  #return it
         else:           #if we do not have text, we don't have an answer
             texts = 'I have no answer for that.'
             texts = texts.encode('ascii', 'ignore')
